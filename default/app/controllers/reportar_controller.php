c30590430dd8c5d7f63d733d408d850d5ca43a84
Jaro Marval <jampgold@gmail.com>
2013-07-02 14:58:32 -0430

    Primera puesta a production

--- default/app/controllers/reportar_controller.php
+++ default/app/controllers/reportar_controller.php
@@ -2,12 +2,33 @@
 
 class ReportarController extends AppController {
 
-    public function index($param1) {
+    public function index($param1=null, $param2=null) {
         $nivel = (isset($param1) && Session::get('nivel') < $param1)?$param1:Session::get('nivel');
-    }
+        $estructura = ( (Session::get('nivel') == $nivel) && (Session::get('estructura') != $param2) )?Session::get('estructura'):$param2;
+        if ($nivel == 5) {
+            Router::redirect('actividades/unidad/'.$estructura);
+        } else {
+            $this->nivel = $nivel;
+            switch ($nivel) {
+                case 1: // Nacional
+                    echo "<h3>Regiones</h3>";
+                    $this->lista = Load::model('region')->listarConActividades();
+                    break;
+                case 2: // Regional
+                    echo "<h3>Distritos</h3>";
+                    $this->lista = Load::model('distrito')->listarConActividades($estructura);
+                    break;
+                case 3: // Distrital
+                    echo "<h3>Grupos</h3>";
+                    $this->lista = Load::model('grupos')->listarConActividades($estructura );
+                    break;
+                case 4: // Grupal
+                    echo "<h3>Unidades</h3>";
+                    $this->lista = Load::model('ramas')->listarConActividades($estructura);
+                    break;
+            }
+        }
 
-    public function estructura() {
-        
     }
 
 }
