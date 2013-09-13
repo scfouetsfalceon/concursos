405196dd5413fe6cbbbea81e144803394e0ae112
Jaro Marval <jampgold@gmail.com>
2013-09-12 23:52:20 -0430

    Agregadas ayudas al sistema

---
+++ default/app/controllers/ayudas_controller.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* Sección de ayudas generales del Sistema
+*/
+class AyudasController extends AppController
+{
+
+    public function index() {
+        // Esto es controlador totalmente vacío
+    }
+
+}
+
+?>
