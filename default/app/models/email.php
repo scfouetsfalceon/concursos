8258203ca81a063327fc51453dc81229dcdc8314
Jaro Marval <jampgold@gmail.com>
2014-01-10 14:25:04 -0430

    Pequeña corrección a los email de soporte

--- default/app/models/email.php
+++ default/app/models/email.php
@@ -34,7 +34,7 @@ class email extends PHPMailer
         $meta .= '<b>Región: </b>'.((!empty($datos->region_nombre))?$datos->region_nombre:'No Aplica').'<br>';
         $meta .= (!empty($datos->distrito_nombre))?'<b>Distrito: </b>'.$datos->distrito_nombre.'<br>':'';
         $meta .= (!empty($datos->grupos_nombre))?'<b>Grupo: </b>'.$datos->grupos_nombre.'<br>':'';
-        $meta .= (!empty($datos->ramas_nombre))?'<b>Rama: </b>'.$datos->rama_snombre.'<br>':'';
+        $meta .= (!empty($datos->ramas_nombre))?'<b>Rama: </b>'.$datos->ramas_nombre.'<br>':'';
         $meta .= '<br><b>Descripción:</b><br>';
         $this->msgHTML($meta.$message);
         $this->Subject = "[Soporte] ".$title;
